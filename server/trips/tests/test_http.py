52749e9e367888f2cd0b4c92f1a6a5e8fdfec294
Chinmay-395 <chinmaydali395@gmail.com>
2020-10-20 22:04:27 +0530

    Added authentication

--- server/trips/tests/test_http.py
+++ server/trips/tests/test_http.py
@@ -1,9 +1,20 @@
+import base64
+import json
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 PASSWORD = 'pAssw0rd!'
+
+
+def create_user(username='user@example.com', password=PASSWORD):
+    return get_user_model().objects.create_user(
+        username=username,
+        first_name='Test',
+        last_name='User',
+        password=password
+    )
 
 
 class AuthenticationTest(APITestCase):
@@ -21,3 +32,29 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['username'], user.username)
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
+
+    def test_user_can_log_in(self):
+        user = create_user()
+        print("**************THE USER MODEL************** \n", user)
+        response = self.client.post(
+            reverse('log_in'),
+            data={
+                'username': user.username,
+                'password': PASSWORD,
+            }
+        )
+
+        print("-------------------------> \n", response.data)
+        access = response.data['access']
+        # access = response.data['access']
+        header, payload, signature = access.split('.')
+        # print("======================================", payload)
+        decoded_payload = base64.b64decode(f'{payload}==')
+        payload_data = json.loads(decoded_payload)
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertIsNotNone(response.data['refresh'])
+        self.assertEqual(payload_data['id'], user.id)
+        self.assertEqual(payload_data['username'], user.username)
+        self.assertEqual(payload_data['first_name'], user.first_name)
+        self.assertEqual(payload_data['last_name'], user.last_name)
--- server/trips/views.py
+++ server/trips/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .serializers import UserSerializer, LogInSerializer
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
