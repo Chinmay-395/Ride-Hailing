c2b289e91d519b235a72e446074b3f2a8507a656
Chinmay-395 <chinmaydali395@gmail.com>
2020-10-22 19:18:00 +0530

    Added the TripView for accessing a user's trips

--- server/trips/urls.py
+++ server/trips/urls.py
@@ -7,4 +7,6 @@ app_name = 'taxi'
 
 urlpatterns = [
     path('', TripView.as_view({'get': 'list'}), name='trip_list'),
+    path('<uuid:trip_id>/',
+         TripView.as_view({'get': 'retrieve'}), name='trip_detail'),
 ]
--- server/trips/views.py
+++ server/trips/views.py
@@ -21,3 +21,11 @@ class TripView(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id'  
+    lookup_url_kwarg = 'trip_id'  
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
