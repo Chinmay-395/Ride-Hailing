ecd054ef7369e4880f523f53deff9d105881a202
Chinmay-395 <chinmaydali395@gmail.com>
2020-11-11 21:58:41 +0530

    section UI support; sub-section Views in part-1 completed

--- server/trips/views.py
+++ server/trips/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.db.models import Q
 
 from .serializers import UserSerializer, LogInSerializer
 from .models import Trip
@@ -18,14 +19,28 @@ class LogInView(TokenObtainPairView):
 
 
 class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id'
+    lookup_url_kwarg = 'trip_id'
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+    def get_queryset(self):
+        """[summary]
+        • [Will fill this in afterwards]
+        """
+        user = self.request.user
+        if user.group == 'driver':
+            return Trip.objects.filter(
+                Q(status=Trip.REQUESTED) | Q(driver=user)
+            )
+        if user.group == 'rider':
+            return Trip.objects.filter(rider=user)
+        return Trip.objects.none()
 
 
 class TripView(viewsets.ReadOnlyModelViewSet):
-    lookup_field = 'id'  
-    lookup_url_kwarg = 'trip_id'  
+    lookup_field = 'id'
+    lookup_url_kwarg = 'trip_id'
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
