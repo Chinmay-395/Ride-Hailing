c8fa358038ed175d93ea5dd0f4ea069fc45d6c8a
Chinmay-395 <chinmaydali395@gmail.com>
2020-11-13 19:32:32 +0530

    React routing completed in part-2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import { Link, Route, Switch } from 'react-router-dom';
+// custom components
+import SignUp from './components/SignUp'; // new
+import LogIn from './components/LogIn';
+// custom css
 import './App.css';
 
 function App() {
   return (
-    <h1>Taxi</h1>
+    <Switch>
+      <Route exact path='/' render={() => (
+        <div>
+          <h1>Taxi</h1>
+          <Link to='/sign-up'>Sign up</Link>
+          <Link to='/log-in'>Log in</Link>
+        </div>
+      )} />
+      <Route path='/sign-up' component={SignUp} />
+      <Route path='/log-in' component={LogIn} />
+    </Switch>
   );
 }
 
