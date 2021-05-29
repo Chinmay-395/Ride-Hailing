d4dbd53473b11805059b8346a6548c51023a307f
Chinmay-395 <chinmaydali395@gmail.com>
2021-05-29 11:34:42 +0530

    final commit before production

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,79 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"; // changed
 import {
   DirectionsRenderer,
   DirectionsService,
   GoogleMap,
   LoadScript,
-  Marker
-} from '@react-google-maps/api';
+  Marker,
+} from "@react-google-maps/api";
 
 function Map(props) {
   const [response, setResponse] = useState(null);
 
-  const hasTwoAddresses = (
-    props.pickUpAddress !== '' &&
-    props.dropOffAddress !== ''
-  );
+  const hasTwoAddresses =
+    props.pickUpAddress !== "" && props.dropOffAddress !== "";
 
-
+  // new
   const directionsCallback = (response) => {
-    if (response !== null && response.status === 'OK') {
+    if (response !== null && response.status === "OK") {
       setResponse(response);
     }
   };
 
-
-
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}
-    >
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
       <GoogleMap
         center={{
           lat: props.lat,
-          lng: props.lng
+          lng: props.lng,
         }}
         mapContainerStyle={{
-          width: '100%',
-          height: '300px',
-          marginBottom: '10px'
+          width: "100%",
+          height: "300px",
+          marginBottom: "10px",
         }}
         zoom={props.zoom}
       >
-        {
-          hasTwoAddresses && (
-            <DirectionsService
-              options={{
-                origin: props.pickUpAddress,
-                destination: props.dropOffAddress,
-                travelMode: 'DRIVING'
-              }}
-              callback={directionsCallback}
-            >
-            </DirectionsService>
-          )
-        }
-        {
-          hasTwoAddresses && response !== null && (
-            <DirectionsRenderer
-              options={{
-                directions: response
-              }}
-            />
-          )
-        }
-        {
-          !hasTwoAddresses && (
-            <Marker
-              label='A'
-              position={{
-                lat: props.lat,
-                lng: props.lng
-              }}
-            >
-            </Marker>
-          )
-        }
+        {/* new */}
+        {hasTwoAddresses && (
+          <DirectionsService
+            options={{
+              origin: props.pickUpAddress,
+              destination: props.dropOffAddress,
+              travelMode: "DRIVING",
+            }}
+            callback={directionsCallback}
+          ></DirectionsService>
+        )}
+        {hasTwoAddresses && response !== null && (
+          <DirectionsRenderer
+            options={{
+              directions: response,
+            }}
+          />
+        )}
+        {!hasTwoAddresses && (
+          <Marker
+            label="A"
+            position={{
+              lat: props.lat,
+              lng: props.lng,
+            }}
+          ></Marker>
+        )}
       </GoogleMap>
     </LoadScript>
   );
--- client/src/components/RiderRequest.js
+++ client/src/components/RiderRequest.js
@@ -1,83 +1,98 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik';
-import {
-  Breadcrumb, Button, Card, Col, Form, Row
-} from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
-
-import { getUser } from '../services/AuthService';
-import { createTrip } from '../services/TripService';
+import React, { useState, useEffect } from "react";
+import { Formik } from "formik";
+import { Breadcrumb, Button, Card, Col, Form, Row } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import Map from "./Map";
+import { getUser } from "../services/AuthService";
+import { createTrip } from "../services/TripService";
 
 function RiderRequest(props) {
   const [isSubmitted, setSubmitted] = useState(false);
+
+  const [lat, setLat] = useState(38.897957);
+  const [lng, setLng] = useState(-77.03656);
+
+  useEffect(() => {
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      });
+    }
+  }, []);
 
   const onSubmit = (values, actions) => {
     const rider = getUser();
     createTrip({
       pick_up_address: values.pickUpAddress,
       drop_off_address: values.dropOffAddress,
-      rider: rider.id
+      rider: rider.id,
     });
     setSubmitted(true);
   };
 
   if (isSubmitted) {
-    return <Redirect to='/rider' />
+    return <Redirect to="/rider" />;
   }
 
   return (
     <Row>
       <Col lg={12}>
         <Breadcrumb>
-          <LinkContainer to='/rider'>
+          <LinkContainer to="/rider">
             <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
           </LinkContainer>
           <Breadcrumb.Item active>Request</Breadcrumb.Item>
         </Breadcrumb>
-        <Card className='mb-3'>
+        <Card className="mb-3">
           <Card.Header>Request Trip</Card.Header>
           <Card.Body>
             <Formik
               initialValues={{
-                pickUpAddress: '',
-                dropOffAddress: ''
+                pickUpAddress: "",
+                dropOffAddress: "",
               }}
               onSubmit={onSubmit}
             >
-              {({
-                handleChange,
-                handleSubmit,
-                isSubmitting,
-                values
-              }) => (
+              {({ handleChange, handleSubmit, isSubmitting, values }) => (
                 <Form noValidate onSubmit={handleSubmit}>
-                  <Form.Group controlId='pickUpAddress'>
+                  <Form.Group controlId="pickUpAddress">
                     <Form.Label>Pick up address:</Form.Label>
                     <Form.Control
-                      data-cy='pick-up-address'
-                      name='pickUpAddress'
+                      data-cy="pick-up-address"
+                      name="pickUpAddress"
                       onChange={handleChange}
                       values={values.pickUpAddress}
                       required
                     />
                   </Form.Group>
-                  <Form.Group controlId='dropOffAddress'>
+                  {/* new */}
+                  <Map
+                    lat={lat}
+                    lng={lng}
+                    zoom={13}
+                    pickUpAddress={values.pickUpAddress}
+                    dropOffAddress={values.dropOffAddress}
+                  />
+                  <Form.Group controlId="dropOffAddress">
                     <Form.Label>Drop off address:</Form.Label>
                     <Form.Control
-                      data-cy='drop-off-address'
-                      name='dropOffAddress'
+                      data-cy="drop-off-address"
+                      name="dropOffAddress"
                       onChange={handleChange}
                       values={values.dropOffAddress}
                     />
                   </Form.Group>
                   <Button
                     block
-                    data-cy='submit'
+                    data-cy="submit"
                     disabled={isSubmitting}
-                    type='submit'
-                    variant='primary'
-                  >Submit</Button>
+                    type="submit"
+                    variant="primary"
+                  >
+                    Submit
+                  </Button>
                 </Form>
               )}
             </Formik>
@@ -85,7 +100,7 @@ function RiderRequest(props) {
         </Card>
       </Col>
     </Row>
-  )
+  );
 }
 
 export default RiderRequest;
